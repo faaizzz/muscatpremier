01eb219b9286ea3aa317fd127d7b3c92709d9a56
Faaiz K Rissad <fffaaiz@gmail.com>
2022-08-28 15:52:39 +0200

    Update contactForm.php

--- php/contactForm.php
+++ php/contactForm.php
@@ -5,9 +5,9 @@ $phone   = $_REQUEST["phone"];
 $patient  = $_REQUEST["patient"];
 $subject   = $_REQUEST["subject"];
 $msg   = $_REQUEST["msg"];
-$to    = "you@yourdomain.com"; // ENTER YOUR EMAIL ADDRESS
+$to    = "info@izmasoft.com"; // ENTER YOUR EMAIL ADDRESS
 if (isset($email) && isset($name) && isset($msg)) {
-    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
+    $email_subject = "$name sent you a message via Muscat Premier Website"; // ENTER YOUR EMAIL SUBJECT
         $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
