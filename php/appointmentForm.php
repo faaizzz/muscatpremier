51cc0aa7cf5adeeb3f68b89c82495532ec2aedb4
Faaiz K Rissad <fffaaiz@gmail.com>
2022-08-28 16:09:31 +0200

    Triggering email for appointment

--- php/appointmentForm.php
+++ php/appointmentForm.php
@@ -1,19 +1,16 @@
 <?
-$department  = $_REQUEST["department"];
 $doctor = $_REQUEST["doctor"];
-$patient  = $_REQUEST["patient"];
 $date = $_REQUEST["date"];
 $name  = $_REQUEST["name"];
 $email = $_REQUEST["email"];
 $phone  = $_REQUEST["phone"];
-$msg   = $_REQUEST["msg"];
-$to    = "you@yourdomain.com"; // ENTER YOUR EMAIL ADDRESS
+$to    = "info@izmasoft.com"; // ENTER YOUR EMAIL ADDRESS
 if (isset($email) && isset($name) && isset($msg)) {
-    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
+    $email_subject = "$name booked an appointment via Muscat Premier Contact Form"; // ENTER YOUR EMAIL SUBJECT
 		$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
-$msg     = "Department: $department <br/> Doctor: $doctor <br/> Patient: $patient <br/> Appointment Date: $date <br/> Patient Name: $name <br/> Patient Email: $email <br/> Patient Phone Number: $phone <br/> Message: $msg";
+$msg     = "Doctor: $doctor <br/> Appointment Date: $date <br/> Patient Name: $name <br/> Patient Email: $email <br/> Patient Phone Number: $phone";
 	
    $mail =  mail($to, $email_subject, $msg, $headers);
   if($mail)
